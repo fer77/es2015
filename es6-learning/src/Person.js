93437d1fa8f5a34d66268d6f295d0397e7a5be63
Fernando Fernandez <fernando.fernandez@chghealthcare.com>
2017-08-11 08:42:01 -0600

    created example to test

---
+++ es6-learning/src/Person.js
@@ -0,0 +1,10 @@
+class Person {
+    constructor(name) {
+        this.name = name;
+    }
+    greet() {
+        return this.name + ' says hello.'; // review template strings.
+    }
+}
+
+console.log(new Person('Fernando').greet());
